252d404ac5febe9abb17e17f6e698e3e517fa728
siillvergun <duejql03@naver.com>
2025-12-30 11:57:23 +0900

    NextToken(), NewToken() 함수 추가 및 go 설정

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "monkey/token"
+
 type Lexer struct{
 	input string
 	position int
@@ -12,6 +14,7 @@ func New(input string) *Lexer {
 	// Lexer 구조체를 하나 만들고 그 주소를 l에 저장
 	// 인자로 받은 input에 값을 넣고, 나머지 구조체 필드는 0 초기화
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
@@ -29,4 +32,37 @@ func (l *Lexer) readChar() {
 	// 다음 인덱스 참조
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token{
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok  = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok  = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok  = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok  = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok  = newToken(token.RBRACE, l.ch)			
+	case ',':
+		tok  = newToken(token.COMMA, l.ch)
+	case '+':
+		tok  = newToken(token.PLUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
